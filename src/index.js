2ee3a7d5e56cc06ba39793f9ba4c5f0267e1cc8d
renox <planetrenox@protonmail.com>
2024-03-01 04:27:32 -0800

    new

--- src/index.js
+++ src/index.js
@@ -1,61 +1,34 @@
 export const OK = 200;
 export const CREATED = 201;
 export const ACCEPTED = 202;
-export const NON_AUTHORITATIVE_INFORMATION = 203;
 export const NO_CONTENT = 204;
-export const RESET_CONTENT = 205;
-export const PARTIAL_CONTENT = 206;
-export const MULTI_STATUS = 207;
 export const ALREADY_REPORTED = 208;
 export const IM_USED = 226;
-
-export const MULTIPLE_CHOICES = 300;
 export const MOVED_PERMANENTLY = 301;
 export const FOUND = 302;
 export const SEE_OTHER = 303;
 export const NOT_MODIFIED = 304;
-export const USE_PROXY = 305;
-
 export const TEMPORARY_REDIRECT = 307;
 export const PERMANENT_REDIRECT = 308;
-
 export const BAD_REQUEST = 400;
 export const UNAUTHORIZED = 401;
 export const PAYMENT_REQUIRED = 402;
 export const FORBIDDEN = 403;
 export const NOT_FOUND = 404;
-export const METHOD_NOT_ALLOWED = 405;
 export const NOT_ACCEPTABLE = 406;
-export const PROXY_AUTHENTICATION_REQUIRED = 407;
 export const REQUEST_TIMEOUT = 408;
 export const CONFLICT = 409;
 export const GONE = 410;
-export const LENGTH_REQUIRED = 411;
-export const PRECONDITION_FAILED = 412;
 export const PAYLOAD_TOO_LARGE = 413;
 export const URI_TOO_LONG = 414;
 export const UNSUPPORTED_MEDIA_TYPE = 415;
 export const RANGE_NOT_SATISFIABLE = 416;
-export const EXPECTATION_FAILED = 417;
-
 export const UNKNOWN_ERROR = 520;
 export const WEB_SERVER_IS_DOWN = 521;
-export const CONNECTION_TIMED_OUT = 522;
-export const ORIGIN_IS_UNREACHABLE = 523;
 export const A_TIMEOUT_OCCURRED = 524;
-export const SSL_HANDSHAKE_FAILED = 525;
-export const INVALID_SSL_CERTIFICATE = 526;
-export const RAILGUN_ERROR = 527;
-
 export const INTERNAL_SERVER_ERROR = 500;
 export const NOT_IMPLEMENTED = 501;
-export const BAD_GATEWAY = 502;
 export const SERVICE_UNAVAILABLE = 503;
-export const GATEWAY_TIMEOUT = 504;
-export const HTTP_VERSION_NOT_SUPPORTED = 505;
-export const VARIANT_ALSO_NEGOTIATES = 506;
 export const INSUFFICIENT_STORAGE = 507;
 export const LOOP_DETECTED = 508;
-export const NOT_EXTENDED = 510;
-export const NETWORK_AUTHENTICATION_REQUIRED = 511;
 
